b74ada2323db9c23b41513f1198aca5efe00098c
GatoMiau03 <d.benaldo03@ufromail.cl>
2024-04-18 09:30:03 -0400

    Primer commit: Metodo para buscar el sismo mas intenso entre los dias pedidos.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
-
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public void mostrarSismoMasIntenso() {
+        double maxIntensidad = lecturas[0][0];
+        int diaMax = 1;
+        int horaMax = 0;
+
+        for (int fila = 0; fila < dias; fila++) {
+            for (int columna = 0; columna < LECTURAS_POR_DIA; columna++) {
+                if (lecturas[fila][columna] > maxIntensidad) {
+                    maxIntensidad = lecturas[fila][columna];
+                    diaMax = fila + 1;
+                    horaMax = columna;
+                }
+            }
+        }
+
+        System.out.printf("\nEl día %d, a las %d:00, se registró el sismo más intenso, con un valor de %.2f en la escala de Richter.", diaMax, horaMax, maxIntensidad);
     }
 }
